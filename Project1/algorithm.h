5ae539f80937359325c30257798b8bfe46af5ebd
dmh258 <dmh258@naver.com>
2024-05-27 00:09:16 +0900

    피보나치 업데이트(추가 작업 필요)

--- Project1/algorithm.h
+++ Project1/algorithm.h
@@ -1,5 +1,8 @@
 int Fibo_main();
 int Exec_Fibo(int Fibo_num);
+// int Make_Fibo_Array(int* array_size);
+void Make_Fibo_Array(int array_size);
+void init_Fibo_Array(int init_array_size);
 int Factorial_main();
 int Exec_Factorial(int factorial_num);
 int Hanoi_main();
